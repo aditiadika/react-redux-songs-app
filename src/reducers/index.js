b6308f38ece3e467b9d0fb0325d1738d712b526e
dika aditia <aditiaaadicka@gmail.com>
2019-11-16 22:18:39 +0700

    in building reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,28 @@
+const songReducer = () => {
+	return [
+		{
+			title: 'No Scrubs',
+			duration: '4:05'
+		},
+		{
+			title: 'I Dont know',
+			duration: '10:05'
+		},
+		{
+			title: 'Lorem Ipsum',
+			duration: '5:00'
+		},
+		{
+			title: 'I Want It Thay Way',
+			duration: '3:00'
+		}
+	];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+	if (action.type === 'SONG_SELECTED') {
+		return action.payload;
+	}
+
+	return selectedSong;
+};
