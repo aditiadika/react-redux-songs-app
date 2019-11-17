254947603236c5c1514e598ed644c4d41ea211fe
dika aditia <aditiaaadicka@gmail.com>
2019-11-17 10:19:54 +0700

    building list with redux data

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Songlist from './SongList';
 
 const App = () => {
 	return (
-		<div>
-			<p>react</p>
+		<div className="ui container grid">
+			<div className="ui row">
+				<div className="column eight wide">
+					<Songlist />
+				</div>
+			</div>
 		</div>
 	);
 };
